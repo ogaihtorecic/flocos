400ab65c9f9b194fb21231cab381533a771e2495
sionise@gmail.com <sionise@gmail.com@6d1d0fe4-a33e-53cc-4956-77463d049018>
2010-08-17 04:40:29 +0000

    Formato dos valores

--- Flocos/grails-app/domain/flocos/Classificacao.groovy
+++ Flocos/grails-app/domain/flocos/Classificacao.groovy
@@ -8,8 +8,8 @@ class Classificacao {
 	static belongsTo = [dadosEducacionais:DadosEducacionais, relatoUso:RelatoUso]
 
     static constraints = {
-    	tipoClassificacao(nullable:false, blank:false, inList:['comunicacao', 'coordenacao', 'colaboracao'])
-    	subcategoria(inList:['', 'assincrona', 'sincrona'])
+    	tipoClassificacao(nullable:false, blank:false, inList:['Comunicação', 'Coordenação', 'Colaboração'])
+    	subcategoria(inList:['', 'Assíncrona', 'Síncrona'])
     	dadosEducacionais(nullable:true)
     	relatoUso(nullable:true)
     }
